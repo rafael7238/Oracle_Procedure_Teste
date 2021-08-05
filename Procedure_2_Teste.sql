fc1fa6e328f7da3ff45adbac938b9885f095881e
rafael7238 <rafael7238@hotmail.com>
2021-08-05 14:40:38 -0300

    Meu Segundo Commit

--- Procedure_2_Teste.sql
+++ Procedure_2_Teste.sql
@@ -3,7 +3,8 @@ create or replace PROCEDURE Consulta_Aluno
   pNome          OUT TALUNO.Nome%TYPE,
   pCep           OUT TALUNO.Cep%TYPE,
   pCidade        OUT TALUNO.Cidade%TYPE,
-  pCidade2        OUT TALUNO.Cidade%TYPE)
+  pCidade2        OUT TALUNO.Cidade%TYPE,
+   pCidade3        OUT TALUNO.Cidade%TYPE)
 IS
 BEGIN
   SELECT nome, Cep, cidade
